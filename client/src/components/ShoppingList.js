4dffd0d09526247e52f080eb34f118276b5a29b2
Victor Lopez <victoralopez07@gmail.com>
2018-08-15 02:15:43 -0500

    Added ShoppingList Component

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -6,10 +6,60 @@ import uuid from 'uuid'
 class ShoppingList extends Component {
   constructor (props) {
     super(props)
-    this.state = { isOpen: false }
+    this.state = {
+      items: [
+        { id: uuid(), name: 'Eggs' },
+        { id: uuid(), name: 'Milk' },
+        { id: uuid(), name: 'Steak' },
+        { id: uuid(), name: 'Water' }
+      ]
+    }
   }
 
-  render () {}
+  render () {
+    const { items } = this.state
+    return (
+      <Container>
+        <Button
+          color='dark'
+          style={{ marginBottom: '2rem' }}
+          onClick={() => {
+            const name = prompt('Enter Item')
+            if (name) {
+              this.setState(prevState => ({
+                items: [...prevState.items, { id: uuid(), name }]
+              }))
+            }
+          }}
+        >
+          Add Item
+        </Button>
+        <ListGroup>
+          <TransitionGroup className='shopping-list'>
+            {items.map(({ id, name }) => (
+              <CSSTransition key={id} timeout={500} classNames='fade'>
+                <ListGroupItem>
+                  <Button
+                    className='remove-btn'
+                    color='danger'
+                    size='sm'
+                    onClick={() => {
+                      this.setState(prevState => ({
+                        items: prevState.items.filter(item => item.id !== id)
+                      }))
+                    }}
+                  >
+                    &times;
+                  </Button>
+                  {name}
+                </ListGroupItem>
+              </CSSTransition>
+            ))}
+          </TransitionGroup>
+        </ListGroup>
+      </Container>
+    )
+  }
 }
 
 export default ShoppingList
