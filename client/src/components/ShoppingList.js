82f367ffaeb4d44b33fe9cd1e39ef9b6745bdf06
Victor Lopez <victoralopez07@gmail.com>
2018-08-14 23:26:27 -0500

    Initial ShoppingList component commit

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import uuid from 'uuid'
+
+class ShoppingList extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { isOpen: false }
+  }
+
+  render () {}
+}
+
+export default ShoppingList
