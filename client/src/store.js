d7447c3c47be610bb25e7699878be84209276e76
Victor Lopez <victoralopez07@gmail.com>
2018-08-19 03:10:06 -0500

    fixed Android bug with Redux dev tools

--- client/src/store.js
+++ client/src/store.js
@@ -4,12 +4,24 @@ import rootReducer from './reducers'
 
 const middleware = [thunk]
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const preloadedState = {}
+
+let store
+
+if (process.env.NODE_ENV === 'production') {
+  store = createStore(rootReducer, compose(applyMiddleware(...middleware)))
+} else {
+  store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(
+        ...middleware
+      ) /* ,
+      // Redux devtools don't work in Android
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__() */
+    )
   )
-)
+}
 
 export default store
